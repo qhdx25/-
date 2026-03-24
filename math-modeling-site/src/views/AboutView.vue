d1bc4ee383afac89fcec9f5e75d7105272e3a60c
qhdx25 <17863694312@163.com>
2026-03-24 17:34:25 +0800

    Add files via upload

---
+++ math-modeling-site/src/views/AboutView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h2>关于我们</h2>
+    <p>
+      南开大学数学建模协会成立于[年份]，是由校内热爱数学建模的同学自发组成的学术社团。
+      我们定期举办建模培训、算法竞赛、经验分享会等活动，为同学们提供交流与成长的平台。
+    </p>
+    <p>
+      协会曾多次组织队伍参加全国大学生数学建模竞赛、“挑战杯”等赛事，并取得优异成绩。
+      欢迎所有对数学建模感兴趣的同学加入我们！
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  padding: 2rem;
+  max-width: 800px;
+  margin: 0 auto;
+  color: #fff;
+  background-color: #242424;
+  min-height: 100vh;
+}
+h2 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+p {
+  font-size: 1.1rem;
+  line-height: 1.6;
+  margin-bottom: 1rem;
+}
+</style>
---
+++ math-modeling-site/src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <h1>南开大学数学建模协会</h1>
+    <p class="intro">
+      我们致力于推广数学建模文化，举办各类赛事与培训活动，
+      帮助同学们提升建模能力与科研思维，欢迎加入我们！
+    </p>
+    <div class="nav-links">
+      <router-link to="/about">关于我们</router-link>
+      <router-link to="/resources">学习资源</router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  text-align: center;
+  padding: 4rem 2rem;
+  min-height: 100vh;
+  background-color: #242424;
+  color: #fff;
+}
+h1 {
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+}
+.intro {
+  font-size: 1.2rem;
+  margin-bottom: 2rem;
+  opacity: 0.9;
+}
+.nav-links a {
+  margin: 0 1rem;
+  padding: 0.6rem 1.2rem;
+  background: #42b983;
+  color: white;
+  text-decoration: none;
+  border-radius: 4px;
+}
+</style>
